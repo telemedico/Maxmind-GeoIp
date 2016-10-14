8a0a1a9d27fb38202113dd9ab9457e13634d14ec
Vladimir Pak <v.pak@roboforex.com>
2016-10-14 08:00:02 +0000

    take directory from config

--- src/Maxmind/Bundle/GeoipBundle/Command/LoadDataCommand.php
+++ src/Maxmind/Bundle/GeoipBundle/Command/LoadDataCommand.php
@@ -5,11 +5,25 @@ namespace Maxmind\Bundle\GeoipBundle\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\Output;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class LoadDataCommand extends Command
 {
+    protected $dataFilePath;
+
+    /**
+     * @param mixed $dataFilePath
+     *
+     * @return self
+     */
+    public function setDataFilePath($dataFilePath)
+    {
+        $this->dataFilePath = $dataFilePath;
+
+        return $this;
+    }
+
     protected function configure()
     {
         $this
@@ -18,9 +32,11 @@ class LoadDataCommand extends Command
             ->addArgument(
                 'source',
                 InputArgument::REQUIRED,
-                'The url source file to download and unzip')
-            ->setHelp(<<<EOT
-The <info>%command.name%</info> command download and install the maxmind geoip data source
+                'The url source file to download and unzip'
+            )
+            ->setHelp(
+                <<<EOT
+                The <info>%command.name%</info> command download and install the maxmind geoip data source
 
 To install the GeoLiteCountry:
 <info>php %command.full_name% http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz</info>
@@ -37,20 +53,23 @@ To install the GeoLite City IPv6 (Beta):
 more information here: http://dev.maxmind.com/geoip/geolite
 
 EOT
-            )
-        ;
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $source = $input->getArgument('source');
-        $bundlePath = $this->getApplication()->getKernel()->getBundle('MaxmindGeoipBundle')->getPath();
-        $dataDir = sprintf('%s', $bundlePath.'/../../../../data/');
+        if ($this->dataFilePath) {
+            $dataDir = dirname($this->dataFilePath);
+        } else {
+            $bundlePath = $this->getApplication()->getKernel()->getBundle('MaxmindGeoipBundle')->getPath();
+            $dataDir = sprintf('%s', $bundlePath.'/../../../../data/');
+        }
         $filename = basename($source);
         $destination = sprintf('%s/%s', $dataDir, $filename);
         $verbose = $output instanceof Output && $output->isVerbose();
         if ($verbose) {
-            $output->writeln(sprintf('Start downloading %s', $source));
+            $output->writeln(sprintf('Start downloading "%s" to "%s"', $source, $destination));
             $output->writeln('...');
         }
         if (!copy($source, $destination)) {
